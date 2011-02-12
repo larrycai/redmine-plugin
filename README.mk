18b9f76bcef58bd664cd76f685a441784506da82
Larry Cai <larry.caiyu@gmail.com>
2011-02-12 23:21:41 +0800

    add README for information

---
+++ README.mk
@@ -0,0 +1,25 @@
+Introduction
+============
+see <http://wiki.jenkins-ci.org/display/JENKINS/Redmine+Plugin> for official information and background
+
+this is just the clone of the Akiko Asami's jenkins redmine plugin from svn codes (https://svn.jenkins-ci.org/trunk/hudson/plugins/redmine/) as Revision 38659.
+
+History data is not imported due to some difficulity
+
+Plan
+----
+1. add support for git 
+2. contribute back to Akika Asami.
+
+Why clone it here
+-----------------
+Sharing with svn repo is quite trick.
+
+ 1. try to contibute the patch, git is the easy way to share code
+ 2. encourage redmine-plugin moving to github to improve
+
+Contact me via larry.caiyu (at) gmail.com
+
+
+
+ 
